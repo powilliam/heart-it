108751a946f914aaea8ec5db200491c54e2ec0c9
William Porto <william.porto@nave.rs>
2021-01-18 10:52:31 -0400

    feat(pictures-with-overlay): adding fake service to display images

---
+++ src/services/pictures.js
@@ -0,0 +1,14 @@
+export const PICTURES_DATA = [
+  {
+    uri:
+      'https://images.unsplash.com/photo-1610917703059-3d8b63ebcfb1?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    uri:
+      'https://images.unsplash.com/photo-1610933530128-c67f9b354068?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    uri:
+      'https://images.unsplash.com/photo-1610871512251-d0febce3f117?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+  },
+];
